99a65f29d8954590cd22b3deccb65899f12ffbb8
Onsel Akin <onsela@outlook.com>
2023-08-08 13:25:09 +0300

    Updated the config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,13 +14,14 @@ const config = {
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'compliance-framework', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
-
+  deploymentBranch: 'gh-pages',
+  
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
